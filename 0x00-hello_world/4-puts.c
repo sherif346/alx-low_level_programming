e18253f37ebe2571138ef05feba96c9df41bb9af
sherif346 <sherifamr777@yahoo.com>
2023-08-10 11:09:25 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: A C program that prints with put function
+ *
+ * Return Always 0 (Success)
+ */
+
+
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+
+
+}
